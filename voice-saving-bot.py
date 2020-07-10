cf00bd7b32147fc0c144f1e457ca8b466ae8e7ef
Vladislav Novitsky <novitsky.vladislav00@gmail.com>
2020-07-11 00:24:15 +0300

    Fixed bugs

--- voice-saving-bot.py
+++ voice-saving-bot.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import CommandHandler, MessageHandler, Filters, CallbackContext, ConversationHandler
 import bot
 
-COMMAND = range(1)
+VOICE, COMMAND = range(2)
 
 
 class VoiceSavingBot(bot.Bot):
@@ -15,28 +15,34 @@ class VoiceSavingBot(bot.Bot):
 	def add_handlers(self, dispatcher):
 		# Command Handlers
 		dispatcher.add_handler(CommandHandler('start', self.start))
-		# Message Handlers
-		dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), self.echo))
+
+		# Conversation Handlers
+		dispatcher.add_handler(ConversationHandler(
+			entry_points=[CommandHandler('save', self.save)],
+
+			states={
+				VOICE: [MessageHandler(Filters.voice, self.voice)],
+
+				COMMAND: [MessageHandler(Filters.text, self.command)]
+			},
+
+			fallbacks=[CommandHandler('cancel', self.cancel)],
+		))
+
+		# Unknown Command Handler
 		dispatcher.add_handler(MessageHandler(Filters.command, self.unknown))
 
-	# Conversation Handlers
-	# dispatcher.add_handler(ConversationHandler(
-	# 	entry_points=[MessageHandler(Filters.voice, self.voice)],
-	#
-	# 	states=[],
-	#
-	# 	fallbacks=[],
-	# ))
+	def save(self, update: Update, context: CallbackContext):
+		update.message.reply_text('Please send me voice message!')
+
+		return VOICE
 
 	def start(self, update: Update, context: CallbackContext):
 		self.logger.info("User %s %s with id: %d started bot", update.effective_user.first_name,
 						 update.effective_user.last_name,
 						 update.effective_user.id)
 		context.bot.send_message(chat_id=update.effective_chat.id,
-								 text="I'm a bot, please talk to me! This is testclass start func")
-
-	def echo(self, update: Update, context: CallbackContext):
-		context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+								 text="I'm a bot, please talk to me!")
 
 	def voice(self, update: Update, context: CallbackContext):
 		voice_id = update.message.voice.file_id
@@ -48,7 +54,7 @@ class VoiceSavingBot(bot.Bot):
 		file_path = 'voice_messages/' + file_name
 		file.download(file_path)
 
-		self.logger_message('sent voice stored as ' + file_name)
+		self.logger_message(update, 'sent voice stored as ' + file_name)
 
 		update.message.reply_text('Voice saved! Please send me a command/codename which you want it to refer to! Or '
 								  'just send /cancel if you changed your mind.')
@@ -56,9 +62,9 @@ class VoiceSavingBot(bot.Bot):
 		return COMMAND
 
 	def command(self, update: Update, context: CallbackContext):
-		command = update.message.from_user
+		command = update.message.text
 
-		self.logger_message(update, 'set command for ' + self.voice_file_name + ' is ' + command)
+		self.logger_message(update, 'set command for ' + self.voice_file_name + ' named ' + command)
 
 		update.message.reply_text(
 			'Voice message is saved! You can access it by sending it with "-" prefix, check it out!')
