74729ec8e4c0ff5206e420d17248710d154488cb
Vladislav Novitsky <novitsky.vladislav00@gmail.com>
2020-07-10 23:40:18 +0300

    Refactored to inherit from Bot class and started voice message conversation

--- voice-saving-bot.py
+++ voice-saving-bot.py
@@ -1,46 +1,37 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, ConversationHandler
-import logging, json, mysql.connector
+from telegram.ext import CommandHandler, MessageHandler, Filters, CallbackContext, ConversationHandler
+import bot
+
+COMMAND = range(1)
 
 
-class VoiceSavingBot:
+class VoiceSavingBot(bot.Bot):
 	def __init__(self):
-		with open("config.json") as json_config:
-			self.data = json.load(json_config)
+		super().__init__()
 
-		self.init_db(self.data)
-
-		self.updater = Updater(token=self.data["telegram"]["token"], use_context=True)
-		self.dispatcher = self.updater.dispatcher
-
-		logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-							level=logging.INFO)
-		self.logger = logging.getLogger(__name__)
-
+		self.voice_file_name = None
 		self.add_handlers(self.dispatcher)
 
-	def enable(self):
-		self.updater.start_polling()
-		self.updater.idle()
-
 	def add_handlers(self, dispatcher):
+		# Command Handlers
 		dispatcher.add_handler(CommandHandler('start', self.start))
 		# Message Handlers
 		dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), self.echo))
 		dispatcher.add_handler(MessageHandler(Filters.command, self.unknown))
 
-		# dispatcher.add_handler(ConversationHandler(
-		# 	entry_points=[MessageHandler(Filters.voice, self.voice)],
-		#
-		# 	states=[],
-		#
-		# 	fallbacks=[],
-		# ))
+	# Conversation Handlers
+	# dispatcher.add_handler(ConversationHandler(
+	# 	entry_points=[MessageHandler(Filters.voice, self.voice)],
+	#
+	# 	states=[],
+	#
+	# 	fallbacks=[],
+	# ))
 
 	def start(self, update: Update, context: CallbackContext):
 		self.logger.info("User %s %s with id: %d started bot", update.effective_user.first_name,
-					update.effective_user.last_name,
-					update.effective_user.id)
+						 update.effective_user.last_name,
+						 update.effective_user.id)
 		context.bot.send_message(chat_id=update.effective_chat.id,
 								 text="I'm a bot, please talk to me! This is testclass start func")
 
@@ -52,33 +43,43 @@ class VoiceSavingBot:
 		user_id = update.effective_user.id
 
 		file = context.bot.getFile(update.message.voice.file_id)
-		file_name = str(user_id) + '__' + voice_id + '.ogg'
+		self.voice_file_name = str(user_id) + '__' + voice_id
+		file_name = self.voice_file_name + '.ogg'
 		file_path = 'voice_messages/' + file_name
-
 		file.download(file_path)
 
-		update.message.reply_text('Voice saved!')
+		self.logger_message('sent voice stored as ' + file_name)
 
-		return 0
+		update.message.reply_text('Voice saved! Please send me a command/codename which you want it to refer to! Or '
+								  'just send /cancel if you changed your mind.')
+
+		return COMMAND
 
 	def command(self, update: Update, context: CallbackContext):
-		void = 'foo'
+		command = update.message.from_user
+
+		self.logger_message(update, 'set command for ' + self.voice_file_name + ' is ' + command)
+
+		update.message.reply_text(
+			'Voice message is saved! You can access it by sending it with "-" prefix, check it out!')
+
+		# todo: SQL save info about voice
+
+		return ConversationHandler.END
+
+	def cancel(self, update: Update, context: CallbackContext):
+		self.logger_message(update, 'cancelled voice saving conversation. File ' + self.voice_file_name + 'will be deleted.')
+
+		update.message.reply_text("Bye! Don't be afraid, voice message is going to be deleted right now!")
+
+		# todo: SQL delete info about voice
+
+		return ConversationHandler.END
 
 	def unknown(self, update: Update, context: CallbackContext):
-		self.logger.info("User %s %s with id: %d tried unknown command: " + update.message.text,
-					update.effective_user.first_name,
-					update.effective_user.last_name, update.effective_user.id)
+		self.logger_message(update, 'tried unknown command: ' + update.message.text)
+
 		context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
-
-	def init_db(self, data):
-		self.db = mysql.connector.connect(
-			host=data["mysql"]["host"],
-			user=data["mysql"]["user"],
-			password=data["mysql"]["password"],
-			database=data["mysql"]["db"]
-		)
-
-		self.db_cursor = self.db.cursor()
 
 
 bot = VoiceSavingBot()
